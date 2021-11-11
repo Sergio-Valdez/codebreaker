eb9b991ddacace8f6c80fc3320cabd30df5de1a1
Sergio Valdez <sergiovaldez159@yahoo.com>
2021-11-11 16:18:27 -0700

    GoogleSignInRepository in progress.

--- app/src/main/java/edu/cnm/deepdive/codebreaker/service/GoogleSignInRepository.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/service/GoogleSignInRepository.java
@@ -1,12 +1,20 @@
 package edu.cnm.deepdive.codebreaker.service;
 
 import android.app.Application;
+import android.content.Intent;
+import android.util.Log;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultLauncher;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 import edu.cnm.deepdive.codebreaker.BuildConfig;
+import io.reactivex.Completable;
 import io.reactivex.Single;
+import io.reactivex.SingleEmitter;
 import io.reactivex.SingleOnSubscribe;
 import io.reactivex.schedulers.Schedulers;
 
@@ -40,7 +48,7 @@ public class GoogleSignInRepository {
 
   public Single<GoogleSignInAccount> refresh() {
     return Single
-        .create((SingleOnSubscribe<GoogleSignInAccount>) (emitter) ->
+        .create((SingleEmitter<GoogleSignInAccount> emitter) ->
             client
                 .silentSignIn()
                 .addOnSuccessListener(this::setAccount)
@@ -50,8 +58,51 @@ public class GoogleSignInRepository {
         .observeOn(Schedulers.io());
   }
 
+  public Single<String> refreshBearerToken() {
+    return refresh()
+        .map(this::getBearerToken);
+  }
+
+  public void startSignIn(ActivityResultLauncher<Intent> launcher) {
+    launcher.launch(client.getSignInIntent());
+  }
+
+  public Single<GoogleSignInAccount> completeSignIn(ActivityResult result) {
+    return Single
+        .create((SingleEmitter<GoogleSignInAccount> emitter) -> {
+          try {
+            Task<GoogleSignInAccount> task =
+                GoogleSignIn.getSignedInAccountFromIntent(result.getData());
+            GoogleSignInAccount account = task.getResult(ApiException.class);
+            setAccount(account);
+            emitter.onSuccess(account);
+          } catch (ApiException e) {
+            emitter.onError(e);
+          }
+        })
+        .observeOn(Schedulers.io());
+  }
+
+  public Completable signOut() {
+    return Completable
+        .create((emitter) ->
+            client
+                .signOut()
+                .addOnCompleteListener((ignored) -> {
+                  setAccount(null);
+                  emitter.onComplete();
+                })
+                .addOnFailureListener(emitter::onError)
+        )
+        .subscribeOn(Schedulers.io());
+  }
+
   private void setAccount(GoogleSignInAccount account) {
     this.account = account;
+    if (account != null) {
+      Log.d(getClass().getSimpleName(),
+          (account.getIdToken() != null) ? getBearerToken(account) : "(none)");
+    }
   }
 
   private String getBearerToken(GoogleSignInAccount account) {
@@ -60,6 +111,7 @@ public class GoogleSignInRepository {
 
   private static class InstanceHolder {
 
+    private static final GoogleSignInRepository INSTANCE = new GoogleSignInRepository();
 
   }
 
